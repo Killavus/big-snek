23d5d545ae33919d7248390bde39489dceba57b8
Killavus <killavus@gmail.com>
2022-09-22 03:05:36 +0200

    crate rename; cleanup of unused components in main

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,6 @@ mod snake;
 
 use snake::SnakeGame;
 
-#[derive(Component)]
-struct Person;
-
-#[derive(Component)]
-struct Name(String);
-
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
